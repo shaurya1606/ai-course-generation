1007b6d2723244dd6df39060849518a07f5d18dc
shaurya1606 <shaurya0616@gmail.com>
2025-11-01 19:54:53 +0530

    constants added for the discussion room

--- apps/letskraack/src/constants/constant.ts
+++ apps/letskraack/src/constants/constant.ts
@@ -121,12 +121,32 @@ export const CoachingExpert = [
     avatar: "J",
   },
   {
-    name: "Sally",
-    avatar: "S",
+    name: "Matthew",
+    avatar: "M",
   },
   {
-    name: "Miame",
-    avatar: "M"
+    name: "Ivy",
+    avatar: "I"
+  },
+  {
+    name: "Justin",
+    avatar: "J"
+  },
+  {
+    name: "Kendra",
+    avatar: "K"
+  },
+  {
+    name: "Salli",
+    avatar: "S"
+  },
+  {
+    name: "Kimberly",
+    avatar: "K"
+  },
+  {
+    name: "Joey",
+    avatar: "J"
   }
 ]
 
@@ -230,6 +250,96 @@ export const resumes: Resume[] = [
       },
     },
   },
+   {
+    id: "4",
+    companyName: "Google",
+    jobTitle: "Frontend Developer",
+    imagePath: "/images/resume_01.png",
+    resumePath: "/resumes/resume-1.pdf",
+    feedback: {
+      overallScore: 85,
+      ATS: {
+        score: 90,
+        tips: [],
+      },
+      toneAndStyle: {
+        score: 90,
+        tips: [],
+      },
+      content: {
+        score: 90,
+        tips: [],
+      },
+      structure: {
+        score: 90,
+        tips: [],
+      },
+      skills: {
+        score: 90,
+        tips: [],
+      },
+    },
+  },
+  {
+    id: "5",
+    companyName: "Microsoft",
+    jobTitle: "Cloud Engineer",
+    imagePath: "/images/resume_02.png",
+    resumePath: "/resumes/resume-2.pdf",
+    feedback: {
+      overallScore: 55,
+      ATS: {
+        score: 90,
+        tips: [],
+      },
+      toneAndStyle: {
+        score: 90,
+        tips: [],
+      },
+      content: {
+        score: 90,
+        tips: [],
+      },
+      structure: {
+        score: 90,
+        tips: [],
+      },
+      skills: {
+        score: 90,
+        tips: [],
+      },
+    },
+  },
+  {
+    id: "6",
+    companyName: "Apple",
+    jobTitle: "iOS Developer",
+    imagePath: "/images/resume_03.png",
+    resumePath: "/resumes/resume-3.pdf",
+    feedback: {
+      overallScore: 75,
+      ATS: {
+        score: 90,
+        tips: [],
+      },
+      toneAndStyle: {
+        score: 90,
+        tips: [],
+      },
+      content: {
+        score: 90,
+        tips: [],
+      },
+      structure: {
+        score: 90,
+        tips: [],
+      },
+      skills: {
+        score: 90,
+        tips: [],
+      },
+    },
+  },
 ];
 
 export const AIResponseFormat = `
@@ -279,11 +389,11 @@ export const AIResponseFormat = `
 export const prepareInstructions = ({
   jobTitle,
   jobDescription,
-  AIResponseFormat,
+ 
 }: {
   jobTitle: string;
   jobDescription: string;
-  AIResponseFormat: string;
+
 }) =>
   `You are an expert in ATS (Applicant Tracking System) and resume analysis.
   Please analyze and rate this resume and suggest how to improve it.
