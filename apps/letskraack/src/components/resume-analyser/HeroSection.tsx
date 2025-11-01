74553789c14d9ed31657472ffff17f18b7511764
shaurya1606 <shaurya0616@gmail.com>
2025-11-01 19:52:37 +0530

    hero componet created

---
+++ apps/letskraack/src/components/resume-analyser/HeroSection.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+
+import { motion } from "motion/react";
+import { Button } from "@/components/ui/button";
+
+export default function HeroSection() {
+  return (
+    <div className="relative mx-auto my-10 flex max-w-7xl flex-col items-center justify-center">
+      <div className="absolute inset-y-0 left-0 h-full w-px bg-neutral-200/80 dark:bg-neutral-800/80">
+        <div className="absolute top-0 h-40 w-px bg-gradient-to-b from-transparent via-blue-500 to-transparent" />
+      </div>
+      <div className="absolute inset-y-0 right-0 h-full w-px bg-neutral-200/80 dark:bg-neutral-800/80">
+        <div className="absolute h-40 w-px bg-gradient-to-b from-transparent via-blue-500 to-transparent" />
+      </div>
+      <div className="absolute inset-x-0 bottom-0 h-px w-full bg-neutral-200/80 dark:bg-neutral-800/80">
+        <div className="absolute mx-auto h-px w-40 bg-gradient-to-r from-transparent via-blue-500 to-transparent" />
+      </div>
+      <div className="px-4 py-10 md:py-20">
+        <h1 className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-slate-700 md:text-4xl lg:text-7xl dark:text-slate-300">
+          {"Track Your Applications & Resume Ratings"
+            .split(" ")
+            .map((word, index) => (
+              <motion.span
+                key={index}
+                initial={{ opacity: 0, filter: "blur(4px)", y: 10 }}
+                animate={{ opacity: 1, filter: "blur(0px)", y: 0 }}
+                transition={{
+                  duration: 0.3,
+                  delay: index * 0.1,
+                  ease: "easeInOut",
+                }}
+                className="mr-2 inline-block"
+              >
+                {word}
+              </motion.span>
+            ))}
+        </h1>
+        <motion.p
+          initial={{
+            opacity: 0,
+          }}
+          animate={{
+            opacity: 1,
+          }}
+          transition={{
+            duration: 0.3,
+            delay: 0.8,
+          }}
+          className="relative z-10 mx-auto max-w-xl py-4 text-center text-lg font-normal text-neutral-600 dark:text-neutral-400"
+        >
+          Review your submissions and check AI-powered feedback.
+        </motion.p>
+        <motion.div
+          initial={{
+            opacity: 0,
+          }}
+          animate={{
+            opacity: 1,
+          }}
+          transition={{
+            duration: 0.3,
+            delay: 1,
+          }}
+          className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
+        >
+          <Button variant='default' className="w-60 transform rounded-lg px-6 py-2 font-medium  transition-all duration-300 hover:-translate-y-0.5">
+            Upload Resume
+          </Button>
+          <Button variant='outline' className="w-60 transform rounded-lg border px-6 py-2 font-medium  transition-all duration-300 hover:-translate-y-0.5">
+            View ATS Scores
+          </Button>
+        </motion.div>
+      </div>
+    </div>
+  );
+}
